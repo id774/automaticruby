d3043c427d8c23b9478859ca975d2cd3fc260c8b
id774 <idnanashi@gmail.com>
2012-02-21 13:53:10 +0900

    13 Use OptionParser, enable specify any recipe file name

--- automatic.rb
+++ automatic.rb
@@ -10,6 +10,35 @@ Dir.glob(File.dirname(__FILE__) + '/plugins/*.rb').each {|r|
   require(File.basename(r, '.rb'))
 }
 
-config = YAML.load(File.open(File.dirname(__FILE__) + '/config/default.yml'))
+filename = ""
+require 'optparse'
+parser = OptionParser.new do |parser|
+  parser.version = "12.02-devel"
+  parser.banner = "#{File.basename($0,".*")}
+  Usage: #{File.basename($0,".*")} [options] arg"
+  parser.separator "options:"
+  parser.on('-c', '--config FILE', String,
+            "read data from FILENAME"){|f| filename = f }
+  parser.on('-h', '--help', "show this message"){
+    puts parser
+    exit
+  }
+end
+
+begin
+  parser.parse!
+rescue OptionParser::ParseError => err
+  $stderr.puts err.message
+  $stderr.puts parser.help
+  exit 1
+end
+
+require 'yaml'
+if filename == ""
+  config = YAML.load(File.open(File.dirname(__FILE__) + '/config/default.yml'))
+else
+  print "Loading #{filename}\n"
+  config = YAML.load(File.open(filename))
+end
 loader = eval(config['plugins']['module']).new(config)
 loader.run
