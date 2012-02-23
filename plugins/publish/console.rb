9aa92b9bb44688cf2efc71d371f71d735315eed0
id774 <idnanashi@gmail.com>
2012-02-23 20:13:13 +0900

    issue3 Plugin Publish::Console added

---
+++ plugins/publish/console.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# Name::      Automatic::Plugin::Publish::Console
+# Author::    774 <http://id774.net>
+# Created::   Feb 23, 2012
+# Updated::   Feb 23, 2012
+# Copyright:: 774 Copyright (c) 2012
+# License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+class PublishConsole
+  require 'pp'
+  attr_accessor :hb
+
+  def initialize(config, pipeline=[])
+    @config = config
+    @pipeline = pipeline
+  end
+
+  def run
+    @pipeline.each {|feeds|
+      unless feeds.nil?
+        feeds.items.each {|feed|
+          pp feed
+        }
+      end
+    }
+    @pipeline
+  end
+end
