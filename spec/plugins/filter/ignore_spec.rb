a7d4b4eb608690ea8af2ba8dbf1409d7ea33e62d
Kazuyuki Ohgushi <kazuyuki.ohgushi@gmail.com>
2012-02-25 01:25:54 +0900

    Add specs for FilterIgnore#exclude

--- spec/plugins/filter/ignore_spec.rb
+++ spec/plugins/filter/ignore_spec.rb
@@ -2,10 +2,53 @@ require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 
 require 'filter/ignore'
 
-describe FilterIgnore do
-  subject { FilterIgnore.new({'exclude' => []}, []) }
+describe Automatic::Plugin::FilterIgnore do
+  context "with invalid argument for #initialize" do
+    context "empty config" do
+      subject { Automatic::Plugin::FilterIgnore.new({}) }
+      
+      specify {
+        lambda { subject.exclude("") }.should raise_exception(
+          NoMethodError, /undefined method `each'/)
+      }
+    end
+  end
+  
+  context "with empty exclusion target" do
+    subject { Automatic::Plugin::FilterIgnore.new({'exclude' => []}, []) }
 
-  specify {
-    subject.exclude("").should be_false
-  }
+    describe "#exclude" do
+      context "for empty link" do
+        specify {
+          subject.exclude("").should be_false
+        }
+      end
+      
+      context "for existent link" do
+        specify {
+          subject.exclude("http://github.com").should be_false
+        }
+      end
+    end
+  end  
+  
+  context "with exclusion target 'github'" do
+    subject {
+      Automatic::Plugin::FilterIgnore.new({'exclude' => ["github"]}, [])
+    }
+    
+    describe "#exclude" do
+      context "for empty link" do
+        specify {
+          subject.exclude("").should be_false
+        }
+      end
+      
+      context "for the link including exclusion targed" do
+        specify {
+          subject.exclude("http://github.com").should be_true
+        }
+      end
+    end
+  end  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,5 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |config|
   
 end
+
+require 'automatic'
