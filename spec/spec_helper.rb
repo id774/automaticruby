1f93d1e6a010e5a8f80d350a835d131126ebf724
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-02-24 09:34:47 +0900

    added require for test group

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'plugins'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'environment'
+Bundler.require :test if defined?(Bundler)
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
