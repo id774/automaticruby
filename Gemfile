a8bddeb1284dc37e1f1e8cba6044f668da5efa87
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-02-24 09:33:41 +0900

    support bundler

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'gcalapi'
+
+group :test do
+  gem 'rspec'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'plugins'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
+require 'environment'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
