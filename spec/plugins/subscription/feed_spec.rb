e4382c681f7319b489fbb1bf522a272ed3f8bbbc
Kazuyuki Ohgushi <kazuyuki.ohgushi@gmail.com>
2012-02-25 17:52:50 +0900

    Add specs of Plugin::SubsucriptionFeed #2

---
+++ spec/plugins/subscription/feed_spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
+
+require 'subscription/feed'
+
+describe Automatic::Plugin::SubscriptionFeed do
+  context "with empty feeds" do
+    subject {
+      Automatic::Plugin::SubscriptionFeed.new(
+        { 'feeds' => [] })
+    }
+    
+    its(:run) { should be_empty }
+  end
+
+  context "with feeds whose invalid URL" do
+    subject {
+      Automatic::Plugin::SubscriptionFeed.new(
+        { 'feeds' => ["invalid_url"] })
+    }
+
+    its(:run) { should be_empty }
+  end
+
+
+  context "with empty feeds" do
+    subject {
+      Automatic::Plugin::SubscriptionFeed.new(
+        { 'feeds' => [
+            "https://github.com/id774/automaticruby/commits/master.atom"]
+        })
+    }
+
+    its(:run) { should have(1).feed }
+  end
+end
+
