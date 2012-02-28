044c635024c87915e6057233f93b0642e46cf4a2
id774 <idnanashi@gmail.com>
2012-02-28 22:27:01 +0900

    54 Revert deletion miss

---
+++ plugins/store/target_link.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# Name::      Automatic::Plugin::Store::TargetLink
+# Author::    774 <http://id774.net>
+# Created::   Feb 28, 2012
+# Updated::   Feb 28, 2012
+# Copyright:: 774 Copyright (c) 2012
+# License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+module Automatic::Plugin
+  class StoreTargetLink
+    require 'open-uri'
+
+    def initialize(config, pipeline=[])
+      @config = config
+      @pipeline = pipeline
+    end
+
+    def wget(url)
+      filename = url.split(/\//).last
+      open(url) do |source|
+        open(File.join(@config['path'], filename), "w+b") do |o|
+          o.print source.read
+        end
+      end
+    end
+
+    def run
+      @pipeline.each {|feeds|
+        unless feeds.nil?
+          feeds.items.each {|feed|
+            unless feed.link.nil?
+              Automatic::Log.puts("info", "Wget: #{feed.link}")
+              wget(feed.link)
+            end
+            sleep @config['interval'].to_i
+          }
+        end
+      }
+      @pipeline
+    end
+  end
+end
