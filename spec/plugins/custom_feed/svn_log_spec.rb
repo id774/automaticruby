9e9d40258b596f8722ea426f01b425b9937dd616
kzgs <kazuyuki.ohgushi@gmail.com>
2012-03-03 00:30:27 +0900

    Modify the way of finding ruby filess related to plugin Module/Class #36

--- spec/plugins/custom_feed/svn_log_spec.rb
+++ spec/plugins/custom_feed/svn_log_spec.rb
@@ -2,13 +2,13 @@
 # Name::      Automatic::Plugin::CustomFeed::SVNFLog
 # Author::    kzgs
 # Created::   Feb 29, 2012
-# Updated::   Feb 29, 2012
+# Updated::   Mar 3, 2012
 # Copyright:: kzgs Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 
-require 'custom/feed_svn_log'
+require 'custom_feed/svn_log'
 
 describe Automatic::Plugin::CustomFeedSVNLog do
   context "with feeds whose valid URL" do
