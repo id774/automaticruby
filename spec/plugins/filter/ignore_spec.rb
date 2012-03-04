0fa1964e43e1079621142b88a706bb64fa191e19
id774 <idnanashi@gmail.com>
2012-03-05 00:25:48 +0900

    75 Update Spec for Filter::Ignore

--- spec/plugins/filter/ignore_spec.rb
+++ spec/plugins/filter/ignore_spec.rb
@@ -3,41 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 require 'filter/ignore'
 
 describe Automatic::Plugin::FilterIgnore do
-  context "with exclusion by title" do
-    subject {
-      Automatic::Plugin::FilterIgnore.new({
-        'title' => ["CD"],
-        'title' => ["DVD"],
-        'title' => ["LIVE"],
-        'title' => ["HMV"],
-      },
-        AutomaticSpec.generate_pipeline {
-          feed { item "http://id774.net/blog/feed/" }
-          feed { item "http://www.hmv.co.jp/rss/news/top/1_100/" }
-        })
-    }
-    
-    describe "#run" do
-      its(:run) { should have(1).feeds }
-    end
-  end
-
   context "with exclusion by description" do
     subject {
       Automatic::Plugin::FilterIgnore.new({
-        'description' => ["CD"],
-        'description' => ["DVD"],
-        'description' => ["LIVE"],
-        'description' => ["HMV"],
+        'description' => ["comment"],
       },
         AutomaticSpec.generate_pipeline {
-          feed { item "http://id774.net/blog/feed/" }
-          feed { item "http://www.hmv.co.jp/rss/news/top/1_100/" }
+          feed { item "http://github.com" }
+          feed { item "http://google.com" }
         })
     }
     
     describe "#run" do
-      its(:run) { should have(1).feeds }
+      its(:run) { should have(2).feeds }
     end
   end
 
