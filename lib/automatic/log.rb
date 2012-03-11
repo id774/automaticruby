6914ddfc5ef64f7b9011e7fcb04c1ac3ff658c6e
kzgs <kazuyuki.ohgushi@gmail.com>
2012-03-11 18:59:27 +0900

    Add subcommand log #54

--- lib/automatic/log.rb
+++ lib/automatic/log.rb
@@ -1,9 +1,8 @@
-#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 # Name::      Automatic::Log
 # Author::    774 <http://id774.net>
 # Created::   Feb 20, 2012
-# Updated::   Feb 24, 2012
+# Updated::   Mar 11, 2012
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -15,10 +14,3 @@ module Automatic
     end
   end
 end
-
-if __FILE__ == $0
-  level = ARGV.shift || abort("Usage: log.rb <level> <message>")
-  message = ARGV.shift
-  Automatic::Log.puts(level, message)
-end
-
