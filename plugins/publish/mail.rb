f5f7c24278f69b2d4abf030e1efc16c3b0c4430c
id774 <idnanashi@gmail.com>
2012-04-05 15:56:10 +0900

    122 Create Plugin Publish::Mail

---
+++ plugins/publish/mail.rb
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Name::      Automatic::Plugin::Publish::Mail
+# Author::    774 <http://id774.net>
+# Created::   Apr 5, 2012
+# Updated::   Apr 5, 2012
+# Copyright:: 774 Copyright (c) 2012
+# License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+module Automatic::Plugin
+  class PublishMail
+    require 'action_mailer'
+
+    def initialize(config, pipeline=[])
+      @config = config
+      @pipeline = pipeline
+    end
+
+    def run
+      Mailer.smtp_settings = default_setting.update(
+        {
+          :port           => @config["port"],
+         #:address        => @config["address"],
+         #:authentication => @config["auth"],
+         #:user_name      => @config["username"],
+         #:password       => @config["password"],
+         #:domain         => @config["domain"],
+        })
+
+      Mailer.raise_delivery_errors = true
+      @pipeline.each { |feeds|
+        unless feeds.nil?
+          feeds.items.each { |feed|
+            Mailer.notify(@config, feed)
+          }
+        end
+      }
+      @pipeline
+    end
+
+    def default_setting
+      return {}
+    end
+
+    class Mailer < ActionMailer::Base
+      def notify(option, feed)
+        m = mail(
+          :date    => Time.now,
+          :subject => feed.title,
+          :to      => option["mailto"],
+         #:cc      => option["mailcc"],
+         #:bcc     => option["mailbcc"],
+          :from    => option["mailfrom"])
+
+        unless feed.content_encoded.nil?
+          m.body = feed.content_encoded
+        else
+          m.body = feed.description
+        end
+
+        m.deliver
+      end
+    end
+  end
+end
