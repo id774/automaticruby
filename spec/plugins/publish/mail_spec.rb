6645f91bac80b4bc0ea4edfff9998bbcaa76e87d
id774 <idnanashi@gmail.com>
2012-04-30 17:35:06 +0900

    127 Test for Plugin Publish::Mail

---
+++ spec/plugins/publish/mail_spec.rb
@@ -0,0 +1,45 @@
+require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
+
+require 'publish/mail'
+require 'mocksmtpd'
+
+describe Automatic::Plugin::PublishMail do
+  before do
+    @pipeline = AutomaticSpec.generate_pipeline {
+      feed { item "http://github.com",
+          'aaa',
+          'bbb\n',
+          'Mon, 30 Jan 2012 13:22:29 +0900'
+      }
+    }
+  end
+
+  subject {
+    Automatic::Plugin::PublishMail.new({
+        "port" => 25,
+        "mailto" => "<to@example.com>",
+        "mailfrom" => "<from@example.com>",
+        "subject" => "test"
+      }, @pipeline)
+  }
+
+  it "should mail to smtp server" do
+    smtpd = SMTPServer.new({
+        :Port => 10025,
+        :MailHook => lambda { |sender| sender.should == "<from@example.com>" },
+        :DataHook => lambda { |tmpf, sender, recipients|
+          tmpf.should include("github.com")
+          sender.should == "<from@example.com>"
+          recipients.should == ["<to@example.com>"]
+        }
+      })
+    Thread.start {
+      smtpd.start
+    }
+    loop do
+      break if smtpd.status == :Running
+    end
+    subject.run
+    smtpd.shutdown
+  end
+end
