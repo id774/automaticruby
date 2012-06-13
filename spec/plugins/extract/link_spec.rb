3139a29dae8fff8fbdf2fb8db1143eca4e9a7702
id774 <idnanashi@gmail.com>
2012-06-13 13:10:30 +0900

    153 Fix typo

--- spec/plugins/extract/link_spec.rb
+++ spec/plugins/extract/link_spec.rb
@@ -23,14 +23,14 @@ describe Automatic::Plugin::ExtractLink do
       its(:run) { should have(4).feeds }
       specify {
         subject.run
-        except = "http://id774.net"
-        subject.instance_variable_get(:@return_html)[0].should == except
-        except = "http://reblog.id774.net"
-        subject.instance_variable_get(:@return_html)[1].should == except
-        except = "http://oh-news.net/live/wp-content/uploads/2011/04/Eila_omote.jpg"
-        subject.instance_variable_get(:@return_html)[2].should == except
-        except = "http://blog.id774.net/post/"
-        subject.instance_variable_get(:@return_html)[3].should == except
+        expect = "http://id774.net"
+        subject.instance_variable_get(:@return_html)[0].should == expect
+        expect = "http://reblog.id774.net"
+        subject.instance_variable_get(:@return_html)[1].should == expect
+        expect = "http://oh-news.net/live/wp-content/uploads/2011/04/Eila_omote.jpg"
+        subject.instance_variable_get(:@return_html)[2].should == expect
+        expect = "http://blog.id774.net/post/"
+        subject.instance_variable_get(:@return_html)[3].should == expect
       }
     end
   end
