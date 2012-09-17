37d1bb39bf6332943c42a3405fe99d29069f3b1e
id774 <idnanashi@gmail.com>
2012-09-18 04:14:14 +0900

    195 RSpec for Subscription::Link

--- spec/plugins/subscription/link_spec.rb
+++ spec/plugins/subscription/link_spec.rb
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# Name::      Automatic::Plugin::Subscription::Twitter
+# Name::      Automatic::Plugin::Subscription::Link
 # Author::    774 <http://id774.net>
-# Created::   Sep 10, 2012
-# Updated::   Sep 10, 2012
+# Created::   Sep 18, 2012
+# Updated::   Sep 18, 2012
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 
-require 'subscription/twitter'
+require 'subscription/link'
 
-describe Automatic::Plugin::SubscriptionTwitter do
+describe Automatic::Plugin::SubscriptionLink do
   context "with empty URLs" do
     subject {
-      Automatic::Plugin::SubscriptionTwitter.new(
+      Automatic::Plugin::SubscriptionLink.new(
         { 'urls' => [] })
     }
     its(:run) { should be_empty }
@@ -21,7 +21,7 @@ describe Automatic::Plugin::SubscriptionTwitter do
 
   context "with URLs whose invalid URL" do
     subject {
-      Automatic::Plugin::SubscriptionTwitter.new(
+      Automatic::Plugin::SubscriptionLink.new(
         { 'urls' => ["invalid_url"] }
       )
     }
@@ -30,9 +30,9 @@ describe Automatic::Plugin::SubscriptionTwitter do
 
   context "with URLs whose valid URL" do
     subject {
-      Automatic::Plugin::SubscriptionTwitter.new(
+      Automatic::Plugin::SubscriptionLink.new(
         { 'urls' => [
-            "http://id774.net/test/twitter/favorites.html"]
+            "http://id774.net"]
         }
       )
     }
