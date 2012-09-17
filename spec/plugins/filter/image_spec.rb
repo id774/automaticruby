feac476b9cbc84dadded67dff58b9b475104e99d
id774 <idnanashi@gmail.com>
2012-09-18 05:38:29 +0900

    198 Refactoring RSpec for Filter Plugins

--- spec/plugins/filter/image_spec.rb
+++ spec/plugins/filter/image_spec.rb
@@ -34,27 +34,27 @@ describe Automatic::Plugin::FilterImage do
 
       specify {
         subject.run
-        subject.instance_variable_get(:@pipeline)[0].items[0].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[0].link.
         should == "http://id774.net/images/link_1.jpg"
-        subject.instance_variable_get(:@pipeline)[0].items[1].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[1].link.
         should == "http://id774.net/images/link_2.jpg"
-        subject.instance_variable_get(:@pipeline)[0].items[2].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[2].link.
         should == "http://id774.net/images/link_3.JPG"
-        subject.instance_variable_get(:@pipeline)[0].items[3].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[3].link.
         should == "http://id774.net/images/link_4.png"
-        subject.instance_variable_get(:@pipeline)[0].items[4].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[4].link.
         should == "http://id774.net/images/link_5.jpeg"
-        subject.instance_variable_get(:@pipeline)[0].items[5].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[5].link.
         should == "http://id774.net/images/link_6.PNG"
-        subject.instance_variable_get(:@pipeline)[0].items[6].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[6].link.
         should be_nil
-        subject.instance_variable_get(:@pipeline)[0].items[7].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[7].link.
         should == "http://id774.net/images/link_8.gif"
-        subject.instance_variable_get(:@pipeline)[0].items[8].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[8].link.
         should == "http://id774.net/images/link_9.GIF"
-        subject.instance_variable_get(:@pipeline)[0].items[9].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[9].link.
         should == "http://id774.net/images/link_10.tiff"
-        subject.instance_variable_get(:@pipeline)[0].items[10].link.
+        subject.instance_variable_get(:@return_feeds)[0].items[10].link.
         should == "http://id774.net/images/link_11.TIFF"
       }
     end
