9fcf29f6870ce737574187814375193038826175
id774 <idnanashi@gmail.com>
2012-09-18 23:40:35 +0900

    208 Write test for the case of feed link including nil

--- spec/plugins/store/permalink_spec.rb
+++ spec/plugins/store/permalink_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Name::      Automatic::Plugin::Store::Link
+# Name::      Automatic::Plugin::Store::Permalink
 # Author::    774 <http://id774.net>
-# Updated::   Jun 14, 2012
+# Updated::   Sep 18, 2012
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -33,6 +33,31 @@ describe Automatic::Plugin::StorePermalink do
     instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
       AutomaticSpec.generate_pipeline {
         feed { item "http://github.com" }
+      }
+    )
+
+    instance.run.should have(1).feed
+    lambda {
+      instance.run.should have(0).feed
+    }.should change(Automatic::Plugin::Permalink, :count).by(0)
+  end
+
+  it "should be considered the case of the feed link nil" do
+    instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
+      AutomaticSpec.generate_pipeline {
+        feed {
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_2.jpg"
+          item "http://id774.net/images/link_3.JPG"
+          item "http://id774.net/images/link_4.png"
+          item "http://id774.net/images/link_5.jpeg"
+          item "http://id774.net/images/link_6.PNG"
+          item nil
+          item "http://id774.net/images/link_8.gif"
+          item "http://id774.net/images/link_9.GIF"
+          item "http://id774.net/images/link_10.tiff"
+          item "http://id774.net/images/link_11.TIFF"
+        }
       }
     )
 
