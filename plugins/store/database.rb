965f902e909a5d01643dbdf80b271fd264b39215
soramugi <soramugi.chika@gmail.com>
2012-12-14 01:40:28 +0900

    Only new feed return_feeds

--- plugins/store/database.rb
+++ plugins/store/database.rb
@@ -2,12 +2,14 @@
 # Name::      Automatic::Plugin::Store::Database
 # Author::    kzgs
 #             774 <http://id774.net>
+#             soramugi <http://soramugi.net>
 # Created::   Feb 27, 2012
-# Updated::   Sep 18, 2012
+# Updated::   Dec 13, 2012
 # Copyright:: kzgs Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 require 'active_record'
+require 'rss'
 
 module Automatic::Plugin
   module Database
@@ -17,22 +19,45 @@ module Automatic::Plugin
       @return_feeds = []
       @pipeline.each {|feeds|
         unless feeds.nil?
-          new_feed = false
+          new_feeds = []
           feeds.items.each {|feed|
             unless feed.link.nil?
               unless existing_records.detect {|b| b.try(unique_key) == feed.link }
                 yield(feed)
-                new_feed = true
+                new_feeds << feed
               end
             end
           }
-          @return_feeds << feeds if new_feed
+          @return_feeds << create_rss(new_feeds)
         end
       }
       @return_feeds
     end
 
     private
+
+    def create_rss(feeds)
+      RSS::Maker.make("2.0") {|maker|
+        xss = maker.xml_stylesheets.new_xml_stylesheet
+        xss.href = "http://www.rssboard.org/rss-specification"
+        maker.channel.about = "http://feeds.rssboard.org/rssboard"
+        maker.channel.title = "Automatic Ruby"
+        maker.channel.description = "Automatic Ruby"
+        maker.channel.link = "http://www.rssboard.org/rss-specification"
+        maker.items.do_sort = true
+        unless feeds.nil?
+          feeds.each {|feed|
+            unless feed.link.nil?
+              item             = maker.items.new_item
+              item.title       = feed.title
+              item.link        = feed.link
+              item.date        = Time.now
+              item.description = "Automatic::Plugin::Store::Database"
+            end
+          }
+        end
+      }
+    end
 
     def create_table
       ActiveRecord::Migration.create_table(model_class.table_name) {|t|
