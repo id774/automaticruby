303b81e494b39f41fe4eec6d3cf916e7d69040a1
id774 <idnanashi@gmail.com>
2012-12-17 13:44:46 +0900

    220 Increase test cases for Store::Database

--- spec/plugins/store/permalink_spec.rb
+++ spec/plugins/store/permalink_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Name::      Automatic::Plugin::Store::Permalink
 # Author::    774 <http://id774.net>
-# Updated::   Sep 18, 2012
+# Updated::   Dec 17, 2012
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -11,14 +11,11 @@ require 'store/permalink'
 require 'pathname'
 
 describe Automatic::Plugin::StorePermalink do
-  before do
+  it "should store 1 record for the new link" do
     @db_filename = "test_permalink.db"
     db_path = Pathname(AutomaticSpec.db_dir).cleanpath+"#{@db_filename}"
     db_path.delete if db_path.exist?
     Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
-  end
-
-  it "should store 1 record for the new link" do
     instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
       AutomaticSpec.generate_pipeline {
         feed { item "http://github.com" }
@@ -30,6 +27,10 @@ describe Automatic::Plugin::StorePermalink do
   end
 
   it "should not store record for the existent link" do
+    @db_filename = "test_permalink.db"
+    db_path = Pathname(AutomaticSpec.db_dir).cleanpath+"#{@db_filename}"
+    db_path.delete if db_path.exist?
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
     instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
       AutomaticSpec.generate_pipeline {
         feed { item "http://github.com" }
@@ -43,6 +44,10 @@ describe Automatic::Plugin::StorePermalink do
   end
 
   it "should be considered the case of the feed link nil" do
+    @db_filename = "test_permalink.db"
+    db_path = Pathname(AutomaticSpec.db_dir).cleanpath+"#{@db_filename}"
+    db_path.delete if db_path.exist?
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
     instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
       AutomaticSpec.generate_pipeline {
         feed {
@@ -68,6 +73,10 @@ describe Automatic::Plugin::StorePermalink do
   end
 
   it "should be considered the case of duplicated links" do
+    @db_filename = "test_permalink.db"
+    db_path = Pathname(AutomaticSpec.db_dir).cleanpath+"#{@db_filename}"
+    db_path.delete if db_path.exist?
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
     instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
       AutomaticSpec.generate_pipeline {
         feed {
@@ -92,7 +101,86 @@ describe Automatic::Plugin::StorePermalink do
     }.should change(Automatic::Plugin::Permalink, :count).by(0)
   end
 
-  it "Only new feed is used when there is new feed." do
+  it "No feed should be generated when there is same feed." do
+    @db_filename = "test_permalink.db"
+    db_path = Pathname(AutomaticSpec.db_dir).cleanpath+"#{@db_filename}"
+    db_path.delete if db_path.exist?
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
+    instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
+      AutomaticSpec.generate_pipeline {
+        feed {
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_3.JPG"
+          item "http://id774.net/images/link_4.png"
+          item "http://id774.net/images/link_5.jpeg"
+          item "http://id774.net/images/link_6.PNG"
+          item nil
+          item "http://id774.net/images/link_8.gif"
+          item "http://id774.net/images/link_9.GIF"
+          item "http://id774.net/images/link_10.tiff"
+          item "http://id774.net/images/link_11.TIFF"
+        }
+      }
+    )
+
+    instance.run.should have(1).feed
+    lambda {
+      instance.run.should have(0).feed
+    }.should change(Automatic::Plugin::Permalink, :count).by(0)
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
+    instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
+      AutomaticSpec.generate_pipeline {
+        feed {
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_3.JPG"
+          item "http://id774.net/images/link_4.png"
+          item "http://id774.net/images/link_5.jpeg"
+          item "http://id774.net/images/link_6.PNG"
+          item nil
+          item "http://id774.net/images/link_8.gif"
+          item "http://id774.net/images/link_9.GIF"
+          item "http://id774.net/images/link_10.tiff"
+          item "http://id774.net/images/link_11.TIFF"
+        }
+      }
+    )
+
+    instance.run.should have(0).feed
+    lambda {
+      instance.run.should have(0).feed
+    }.should change(Automatic::Plugin::Permalink, :count).by(0)
+  end
+
+  it "Only new feed should be generated when there is new feed." do
+    @db_filename = "test_permalink.db"
+    db_path = Pathname(AutomaticSpec.db_dir).cleanpath+"#{@db_filename}"
+    db_path.delete if db_path.exist?
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
+    instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
+      AutomaticSpec.generate_pipeline {
+        feed {
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_1.jpg"
+          item "http://id774.net/images/link_3.JPG"
+          item "http://id774.net/images/link_4.png"
+          item "http://id774.net/images/link_5.jpeg"
+          item "http://id774.net/images/link_6.PNG"
+          item nil
+          item "http://id774.net/images/link_8.gif"
+          item "http://id774.net/images/link_9.GIF"
+          item "http://id774.net/images/link_10.tiff"
+          item "http://id774.net/images/link_11.TIFF"
+        }
+      }
+    )
+
+    instance.run.should have(1).feed
+    lambda {
+      instance.run.should have(0).feed
+    }.should change(Automatic::Plugin::Permalink, :count).by(0)
+    Automatic::Plugin::StorePermalink.new({"db" => @db_filename}).run
     instance = Automatic::Plugin::StorePermalink.new({"db" => @db_filename},
       AutomaticSpec.generate_pipeline {
         feed {
