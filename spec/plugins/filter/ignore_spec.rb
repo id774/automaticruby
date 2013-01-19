fbd9be81d72b7bb996cc701b78ebb0f498781d8f
id774 <idnanashi@gmail.com>
2013-01-19 14:51:41 +0900

    229 Append filter tests

--- spec/plugins/filter/ignore_spec.rb
+++ spec/plugins/filter/ignore_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Name::      Automatic::Plugin::Filter::Ignore
 # Author::    774 <http://id774.net>
-# Updated::   Jun 14, 2012
+# Updated::   Jan 19, 2013
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -10,7 +10,7 @@ require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 require 'filter/ignore'
 
 describe Automatic::Plugin::FilterIgnore do
-  context "with exclusion by description" do
+  context "with exclusion by links" do
     subject {
       Automatic::Plugin::FilterIgnore.new({
         'exclude' => ["comment"],
@@ -26,7 +26,55 @@ describe Automatic::Plugin::FilterIgnore do
     end
   end
 
-  context "with exclusion by description" do
+  context "with exclusion by links" do
+    subject {
+      Automatic::Plugin::FilterIgnore.new({
+        'exclude' => ["comment"],
+      },
+        AutomaticSpec.generate_pipeline {
+          feed { item "http://github.com" }
+          feed { item "http://google.com/comment" }
+        })
+    }
+
+    describe "#run" do
+      its(:run) { should have(1).feeds }
+    end
+  end
+
+  context "with exclusion by links" do
+    subject {
+      Automatic::Plugin::FilterIgnore.new({
+        'link' => ["hoge"],
+      },
+        AutomaticSpec.generate_pipeline {
+          feed { item "http://github.com" }
+          feed { item "http://google.com" }
+        })
+    }
+
+    describe "#run" do
+      its(:run) { should have(2).feeds }
+    end
+  end
+
+  context "with exclusion by links" do
+    subject {
+      Automatic::Plugin::FilterIgnore.new({
+        'link' => ["hoge"],
+      },
+        AutomaticSpec.generate_pipeline {
+          feed { item "http://github.com/hoge" }
+          feed { item "http://google.com" }
+        })
+    }
+
+    describe "#run" do
+      its(:run) { should have(1).feeds }
+    end
+  end
+
+  context "with exclusion by links" do
     subject {
       Automatic::Plugin::FilterIgnore.new({
         'exclude' => [""],
@@ -74,7 +122,7 @@ describe Automatic::Plugin::FilterIgnore do
     end
   end
 
-  context "with exclusion by link" do
+  context "with exclusion by links" do
     subject {
       Automatic::Plugin::FilterIgnore.new({
         'link' => ["github"],
@@ -90,7 +138,7 @@ describe Automatic::Plugin::FilterIgnore do
     end
   end
 
-  context "with exclusion by link" do
+  context "with exclusion by links" do
     subject {
       Automatic::Plugin::FilterIgnore.new({
         'link' => [""],
