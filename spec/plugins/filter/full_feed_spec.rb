4ebc4ad7c9f2f8087f8fab2e0a106ca39316f64d
id774 <idnanashi@gmail.com>
2013-03-24 23:56:47 +0900

    285 Append spec for Filter::FullFeed

--- spec/plugins/filter/full_feed_spec.rb
+++ spec/plugins/filter/full_feed_spec.rb
@@ -2,7 +2,7 @@
 # Name::      Automatic::Plugin::Filter::FullFeed
 # Author::    774 <http://id774.net>
 # Created::   Jan 24, 2013
-# Updated::   Feb 15, 2013
+# Updated::   Mar 24, 2013
 # Copyright:: 774 Copyright (c) 2013
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -77,4 +77,55 @@ describe Automatic::Plugin::FilterFullFeed do
     end
   end
 
+  context "It should be not matched by siteinfo with local dir" do
+    subject {
+      Automatic::Plugin::FilterFullFeed.new(
+        {
+          'siteinfo' => "items_all.json"
+        },
+        AutomaticSpec.generate_pipeline {
+          feed {
+            item "http://id774.net", "cccc",
+            "ddddd",
+            "Mon, 07 Mar 2011 15:54:11 +0900"
+          }})}
+
+    describe "#run" do
+      def cleanup_dir
+        root_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", ".."))
+        dir = (File.expand_path('~/.automatic/assets/siteinfo'))
+        if File.directory?(dir)
+          puts "Removing #{dir}"
+          FileUtils.rm_r(dir)
+        end
+        return dir, root_dir
+      end
+
+      before do
+        dir, root_dir = cleanup_dir
+        FileUtils.mkdir_p(dir)
+        FileUtils.cp_r(root_dir + '/assets/siteinfo/items_all.json', dir)
+      end
+
+      its(:run) { should have(1).feeds }
+
+      specify {
+        subject.instance_variable_get(:@pipeline)[0].items[0].link.
+        should == "http://id774.net"
+        subject.instance_variable_get(:@pipeline)[0].items[0].description.
+        should == "ddddd"
+
+        subject.run
+
+        subject.instance_variable_get(:@pipeline)[0].items[0].link.
+        should == "http://id774.net"
+        subject.instance_variable_get(:@pipeline)[0].items[0].description.
+        should == "ddddd"
+      }
+
+      after do
+        cleanup_dir
+      end
+    end
+  end
 end
