4b40a0b57629e2a38e2395bc0567ce4525ee550f
soramugi <soramugi.chika@gmail.com>
2013-06-02 15:07:56 +0900

    Add plugin console_link

---
+++ plugins/publish/console_link.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Name::      Automatic::Plugin::Publish::ConsoleLink
+# Author::    soramugi <http://soramugi.net>
+# Created::   Jun 02, 2013
+# Updated::   Jun 02, 2013
+# Copyright:: soramugi Copyright (c) 2013
+# License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+module Automatic::Plugin
+  class PublishConsoleLink
+    require 'pp'
+
+    def initialize(config, pipeline=[])
+      @config = config
+      @pipeline = pipeline
+      @output = STDOUT
+    end
+
+    def run
+      @pipeline.each { |feeds|
+        unless feeds.nil?
+          feeds.items.each { |feed|
+            @output.puts("info", feed.link)
+          }
+        end
+      }
+      @pipeline
+    end
+  end
+end
