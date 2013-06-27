e0b2fabfe66c57b23845099dd445ecac72b0477b
soramugi <soramugi.chika@gmail.com>
2013-06-28 04:28:47 +0900

    Add plugins CHAN-TORU

---
+++ plugins/subscription/chan_toru.rb
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Name::      Automatic::Plugin::Subscription::ChanToru
+# Author::    soramugi <http://soramugi.net>
+# Created::   Jun 28, 2013
+# Updated::   Jun 28, 2013
+# Copyright:: Copyright (c) 2013 Automatic Ruby Developers.
+# License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+require File.expand_path(File.dirname(__FILE__) + '/g_guide')
+
+module Automatic::Plugin
+  class SubscriptionChanToru
+
+    def initialize(config, pipeline=[])
+      @config   = config
+      @pipeline = pipeline
+    end
+
+    def g_guide_pipeline
+      SubscriptionGGuide.new(@config, @pipeline).run
+    end
+
+    def run
+      retries = 0
+      begin
+        pipeline = g_guide_pipeline
+        pipeline.each {|feeds|
+          feeds.items.each {|feed|
+            feed = link_change(feed)
+          }
+        }
+        @pipeline = pipeline
+      rescue
+        retries += 1
+        Automatic::Log.puts("error", "ErrorCount: #{retries}, Fault in parsing: #{retries}")
+        sleep @config['interval'].to_i unless @config['interval'].nil?
+        retry if retries <= @config['retry'].to_i unless @config['retry'].nil?
+      end
+
+      @pipeline
+    end
+
+    def link_change(feed)
+      feed.link.gsub(/([0-9]+)/) do |pid|
+        if pid != ''
+          feed.link = "https://tv.so-net.ne.jp/chan-toru/intent" +
+            "?cat=1&area=23&pid=#{pid}&from=tw"
+        else
+          feed.link = nil
+        end
+      end
+      feed
+    end
+
+  end
+end
---
+++ spec/plugins/subscription/chan_toru_spec.rb
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# Name::      Automatic::Plugin::Subscription::g_guide
+# Author::    soramugi <http://soramugi.net>
+# Created::   May 21, 2013
+# Updated::   May 21, 2013
+# Copyright:: Copyright (c) 2012-2013 Automatic Ruby Developers.
+# License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
+
+require 'subscription/chan_toru'
+
+def chan_toru(config = {}, pipeline = [])
+  Automatic::Plugin::SubscriptionChanToru.new(config,pipeline)
+end
+
+describe 'Automatic::Plugin::SubscriptionChanToru' do
+  context 'when feed is empty' do
+
+    describe 'attestation error' do
+      subject { chan_toru }
+      before do
+        subject.should_receive(:g_guide_pipeline).times.and_return([])
+      end
+      its(:run) { should be_empty }
+    end
+
+    describe 'interval & retry was used error' do
+      config = {'interval' => 1, 'retry' => 1}
+      subject { chan_toru(config) }
+      before do
+        subject.
+          should_receive(:g_guide_pipeline).
+          exactly(2).
+          times.and_return('')
+      end
+      its(:run) { should be_empty }
+    end
+
+  end
+
+  context 'when feed' do
+    describe 'config keyword' do
+      subject { chan_toru }
+      before do
+        pipeline = AutomaticSpec.generate_pipeline {
+          feed {
+          item "http://soramugi.net/images/hugehuge"
+          item "http://tv.so-net.ne.jp/schedule/500333201307052330.action?from=rss"
+        }}
+        subject.
+          should_receive(:g_guide_pipeline).
+          times.and_return(pipeline)
+      end
+      its(:run) { should have(1).feed }
+    end
+
+  end
+end
