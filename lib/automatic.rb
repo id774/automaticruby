ab3b8b95bd741f0ab3c1a4f4246f750f9af9b7a6
id774 <idnanashi@gmail.com>
2013-12-18 15:48:42 +0900

    Version bump to next release

--- lib/automatic.rb
+++ lib/automatic.rb
@@ -2,7 +2,7 @@
 # Name::      Automatic::Ruby
 # Author::    774 <http://id774.net>
 # Created::   Feb 18, 2012
-# Updated::   Dec 17, 2013
+# Updated::   Dec 18, 2013
 # Copyright:: Copyright (c) 2012-2013 Automatic Ruby Developers.
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -13,7 +13,7 @@ module Automatic
   require 'automatic/log'
   require 'automatic/feed_parser'
 
-  VERSION = "13.12.0"
+  VERSION = "13.12.0-devel"
   USER_DIR = "/.automatic"
 
   class << self
