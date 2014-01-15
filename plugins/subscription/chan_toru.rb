0fcf960b36e2fae9de52459e6612c87a0d03d34d
id774 <idnanashi@gmail.com>
2014-01-15 15:03:12 +0900

    #65 Improvement CI with network connection

--- plugins/subscription/chan_toru.rb
+++ plugins/subscription/chan_toru.rb
@@ -2,8 +2,8 @@
 # Name::      Automatic::Plugin::Subscription::ChanToru
 # Author::    soramugi <http://soramugi.net>
 # Created::   Jun 28, 2013
-# Updated::   Jun 28, 2013
-# Copyright:: Copyright (c) 2012-2013 Automatic Ruby Developers.
+# Updated::   Jan 15, 2014
+# Copyright:: Copyright (c) 2012-2014 Automatic Ruby Developers.
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 require File.expand_path(File.dirname(__FILE__) + '/g_guide')
@@ -22,6 +22,7 @@ module Automatic::Plugin
 
     def run
       retries = 0
+      retry_max = @config['retry'].to_i || 0
       begin
         pipeline = g_guide_pipeline
         pipeline.each {|feeds|
@@ -32,7 +33,6 @@ module Automatic::Plugin
         @pipeline = pipeline
       rescue
         retries += 1
-        retry_max = @config['retry'].to_i || 0
         Automatic::Log.puts("error", "ErrorCount: #{retries}, Fault in parsing: #{retries}")
         sleep ||= @config['interval'].to_i
         retry if retries <= retry_max
