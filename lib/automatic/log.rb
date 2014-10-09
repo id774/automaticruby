c2fd99fdd242d0918d30ee14dffef325f0e9e9eb
id774 <idnanashi@gmail.com>
2014-10-09 18:55:04 +0900

    #124 Using Logger

--- lib/automatic/log.rb
+++ lib/automatic/log.rb
@@ -2,9 +2,11 @@
 # Name::      Automatic::Log
 # Author::    774 <http://id774.net>
 # Created::   Feb 20, 2012
-# Updated::   Mar 11, 2012
-# Copyright:: Copyright (c) 2012-2013 Automatic Ruby Developers.
+# Updated::   Oct 09, 2014
+# Copyright:: Copyright (c) 2012-2014 Automatic Ruby Developers.
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
+
+require 'logger'
 
 module Automatic
   module Log
@@ -14,13 +16,16 @@ module Automatic
       @level = level
     end
 
-    def self.puts(level, message)
+    def self.logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
+    def self.puts(level = :info, message)
       if LOG_LEVELS.index(@level).to_i > LOG_LEVELS.index(level).to_i
         return
       end
-      t = Time.now.strftime("%Y/%m/%d %X")
-      print log = "#{t} [#{level}] #{message}\n"
-      return log
+      logger.send(level, message)
     end
+
   end
 end
