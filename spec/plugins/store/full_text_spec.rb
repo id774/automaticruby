94aac7f3f1077126a43c3e1f04982da47a45204c
id774 <idnanashi@gmail.com>
2014-10-15 18:53:24 +0900

    379 Append tests for Store::FullText

--- spec/plugins/store/full_text_spec.rb
+++ spec/plugins/store/full_text_spec.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Name::      Automatic::Plugin::Store::FullText
 # Author::    774 <http://id774.net>
-# Updated::   Jun 14, 2012
-# Copyright:: Copyright (c) 2012-2013 Automatic Ruby Developers.
+# Updated::   Oct 15, 2014
+# Copyright:: Copyright (c) 2012-2014 Automatic Ruby Developers.
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
@@ -35,9 +35,11 @@ describe Automatic::Plugin::StoreFullText do
       }
     )
 
+    Automatic::Plugin::Blog.count.should eq 0
     lambda {
       instance.run.should have(1).feed
     }.should change(Automatic::Plugin::Blog, :count).by(1)
+    Automatic::Plugin::Blog.count.should eq 1
   end
 
   it "should not store record for the existent blog entry" do
@@ -47,10 +49,55 @@ describe Automatic::Plugin::StoreFullText do
       }
     )
 
+    Automatic::Plugin::Blog.count.should eq 0
     instance.run.should have(1).feed
     lambda {
       instance.run.should have(0).feed
     }.should change(Automatic::Plugin::Blog, :count).by(0)
+    instance.run.should have(0).feed
+    Automatic::Plugin::Blog.count.should eq 1
+  end
+
+  it "should not store record for the existent blog link" do
+    instance = Automatic::Plugin::StoreFullText.new({"db" => @db_filename},
+      AutomaticSpec.generate_pipeline {
+        feed {
+          item "http://blog.id774.net/post/100",
+          "#{rand(10000000)}",
+          "aaa bbb ccc http://test2.id774.net ddd eee",
+          "Mon, 07 Mar 2011 15:54:11 +0900"
+        }
+      }
+    )
+
+    Automatic::Plugin::Blog.count.should eq 0
+    instance.run.should have(1).feed
+    lambda {
+      instance.run.should have(0).feed
+    }.should change(Automatic::Plugin::Blog, :count).by(0)
+    instance.run.should have(0).feed
+    Automatic::Plugin::Blog.count.should eq 1
+  end
+
+  it "should not store record for the existent blog title" do
+    instance = Automatic::Plugin::StoreFullText.new({"db" => @db_filename},
+      AutomaticSpec.generate_pipeline {
+        feed {
+          item "http://blog.id774.net/post/#{rand(1000000)}",
+          "dummy",
+          "aaa bbb ccc http://test2.id774.net ddd eee",
+          "Mon, 07 Mar 2011 15:54:11 +0900"
+        }
+      }
+    )
+
+    Automatic::Plugin::Blog.count.should eq 0
+    instance.run.should have(1).feed
+    lambda {
+      instance.run.should have(0).feed
+    }.should change(Automatic::Plugin::Blog, :count).by(0)
+    instance.run.should have(0).feed
+    Automatic::Plugin::Blog.count.should eq 1
   end
 
 end
